eecb8266ed59ce40c90cc0575450848b02f73598
Lizardz7z <lisa.lis.s@mail.ru>
2023-04-12 17:33:48 +0300

    Test1 is ready

--- quotes.py
+++ quotes.py
@@ -1,16 +1,22 @@
 from playwright.sync_api import sync_playwright
 import selectors
 
+def Test1(page, number):
+    page.go('https://impresso-expresso.netlify.app/')
+    page.go_products()
+    page1 = selectors.Products_Page(page.page)
+    page1.add_to_cart('//section/div/div/div/div/div/div/button', number)
+    page1.go_cart()
+    page1.check_prices('//*[@id="snipcart-items-list"]/tr/td[5]/span', number)
+
+
 def main():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
 
         page = selectors.Home_Page(browser.new_page())
-        page.go('https://impresso-expresso.netlify.app/')
-        page.go_products()
-        page.wait(5000)
-        page1=selectors.Products_Page(page.page)
-        page1.add_to_cart('//section/div/div/div/div/div/div/button', 3)
+        Test1(page, 3)
+
         browser.close()
 
 if __name__=='__main__':
--- selectors.py
+++ selectors.py
@@ -10,6 +10,10 @@ class BasePage:
     def follow_link(self, selector):
         link=self.page.query_selector(selector)
         link.click()
+        self.page.wait_for_timeout(2000)
+
+    def go_cart(self):
+        self.follow_link('//*[@id="gatsby-focus-wrapper"]/nav/div/ul/li[6]')
 
     def wait(self, timeout):
         self.page.wait_for_timeout(timeout)
@@ -22,10 +26,25 @@ class Products_Page(BasePage):
     def add_to_cart(self, selector, number):
         products = self.page.query_selector_all(selector)
         close_button = self.page.query_selector('//*[@id="snipcart-close"]/i')
-        for i in range(0,number):
+        for i in range(0, number):
             products[i].click()
-            self.wait(4000)
-            close_button.click()
             self.wait(2000)
+            close_button.click()
+            self.wait(1000)
 
-
+    def check_prices(self, selector, number):
+        prices=self.page.query_selector_all(selector)
+        sum=0.0
+        for i in range(0, number):
+            s=prices[0].inner_text()
+            sn=s[1:]
+            a=float(sn)
+            sum+=a
+        sumofproducts=self.page.query_selector('//*[@id="snipcart-amount"]')
+        s1=sumofproducts.inner_text()
+        s1n=s1[1:]
+        a1=float(s1n)
+        if (sum==a1):
+            print("Test1 is Correct!")
+        else:
+            print("Test1 is Incorrect :(")
