431e4abb700572d03ba6c67ab0f6ccade0ad7ec9
Lizardz7z <lisa.lis.s@mail.ru>
2023-04-26 20:40:00 +0300

    Подключена отчетность с помощью allure

--- tests/test_cart_functionality.py
+++ tests/test_cart_functionality.py
@@ -1,34 +1,42 @@
 from selectrs.locators import *
 import re
+import pytest
+import allure
 
-
+@allure.feature("Cart functionality checking")
+@allure.feature("Check goods adding to the cart")
+@allure.story("Проверка функции добавления товаров в корзину")
 def test_add_to_cart(setup_browser):
-    page = setup_browser
-    link = page.query_selector(selectors_products_page.PRODUCTS_PAGE_LINK)
-    link.click()
-    page.wait_for_timeout(2000)
-    products = page.query_selector_all(selectors_products_page.ITEMS_SELECTOR)
-    our_products = {products[i].get_attribute(selectors_cart_page.DATA_ITEM_NAME_ATTRIBUTE).upper(): 0
-                    for i in range(len(products))}
-    for i in range(len(products)):
-        products[i].click()
-        our_products[products[i].get_attribute(selectors_cart_page.DATA_ITEM_NAME_ATTRIBUTE).upper()] += 1
+    with allure.step("Запуск браузера и открытие страницы"):
+        page = setup_browser
+    with allure.step("Переход на страницу 'продукты'"):
+        link = page.query_selector(selectors_products_page.PRODUCTS_PAGE_LINK)
+        link.click()
         page.wait_for_timeout(2000)
-        close_button = page.query_selector(selectors_cart_page.CART_CLOSE_BUTTON)
-        close_button.click()
-        page.wait_for_timeout(1000)
-    products[3].click()
-    our_products[products[3].get_attribute(selectors_cart_page.DATA_ITEM_NAME_ATTRIBUTE).upper()] += 1
-    cart_link = page.query_selector(selectors_cart_page.CART_PAGE_LINK)
-    cart_link.click()
-    page.wait_for_timeout(2000)
-    products_in_cart = page.query_selector_all(selectors_cart_page.ITEMS_IN_CART)
-    products_names_in_cart = {}
-    for i in range(len(products_in_cart)):
-        products_names_in_cart[products_in_cart[i].query_selector(selectors_cart_page.PRODUCT_NAME).inner_text()] = \
-            int(products_in_cart[i].query_selector(selectors_cart_page.QUANTITY).inner_text())
-    for i in our_products.items():
-        assert products_names_in_cart[i[0]] == i[1], "Products added to cart incorrectly"
+    with allure.step("Добавление товаров в корзину"):
+        products = page.query_selector_all(selectors_products_page.ITEMS_SELECTOR)
+        our_products = {products[i].get_attribute(selectors_cart_page.DATA_ITEM_NAME_ATTRIBUTE).upper(): 0
+                        for i in range(len(products))}
+        for i in range(len(products)):
+            products[i].click()
+            our_products[products[i].get_attribute(selectors_cart_page.DATA_ITEM_NAME_ATTRIBUTE).upper()] += 1
+            page.wait_for_timeout(2000)
+            close_button = page.query_selector(selectors_cart_page.CART_CLOSE_BUTTON)
+            close_button.click()
+            page.wait_for_timeout(1000)
+        products[3].click()
+        our_products[products[3].get_attribute(selectors_cart_page.DATA_ITEM_NAME_ATTRIBUTE).upper()] += 1
+        cart_link = page.query_selector(selectors_cart_page.CART_PAGE_LINK)
+        cart_link.click()
+        page.wait_for_timeout(2000)
+        products_in_cart = page.query_selector_all(selectors_cart_page.ITEMS_IN_CART)
+        products_names_in_cart = {}
+    with allure.step("Проверка правильности добавления товаров"):
+        for i in range(len(products_in_cart)):
+            products_names_in_cart[products_in_cart[i].query_selector(selectors_cart_page.PRODUCT_NAME).inner_text()] = \
+                int(products_in_cart[i].query_selector(selectors_cart_page.QUANTITY).inner_text())
+        for i in our_products.items():
+            assert products_names_in_cart[i[0]] == i[1], "Products added to cart incorrectly"
 
 
 # def test_check_prices(setup_browser):
--- tests/test_navigation_menu.py
+++ tests/test_navigation_menu.py
@@ -1,52 +1,84 @@
 from selectrs.locators import *
+import pytest
+import allure
 
-
+@allure.feature("Navigation menu functionality checking")
 class Tests:
+    @allure.feature("Move to home page throw navigation menu")
+    @allure.story("Переход на домашнюю страницу с помощью навигационного меню")
     def test_go_to_home_page(self, setup_browser):
-        page = setup_browser
-        link = page.query_selector(selectors_base_page.HOME_PAGE_LINK)
-        link.click()
-        page.wait_for_timeout(2000)
-        title = page.query_selector(selectors_base_page.HOME_PAGE_TITLE)
-        assert title.inner_text() == "IMPRESSO ESPRESSO", "Link to home page is not working"
+        with allure.step("Запуск браузера и открытие страницы"):
+            page = setup_browser
+        with allure.step("Переход на домашнюю страницу"):
+            link = page.query_selector(selectors_base_page.HOME_PAGE_LINK)
+            link.click()
+            page.wait_for_timeout(2000)
+        with allure.step("Проверка правильности перехода"):
+            title = page.query_selector(selectors_base_page.HOME_PAGE_TITLE)
+            assert title.inner_text().upper() == "IMPRESSO ESPRESSO", "Link to home page is not working"
 
+    @allure.feature("Move to menu page throw navigation menu")
+    @allure.story("Переход на страницу 'меню' с помощью навигационного меню")
     def test_go_to_our_menu_page(self, setup_browser):
-        page = setup_browser
-        link = page.query_selector(selectors_menu_page.OUR_MENU_PAGE_LINK)
-        link.click()
-        page.wait_for_timeout(2000)
-        title = page.query_selector(selectors_menu_page.OUR_MENU_PAGE_TITLE)
-        assert title.inner_text() == "OUR MENU", "Link to our menu page is not working"
+        with allure.step("Запуск браузера и открытие страницы"):
+            page = setup_browser
+        with allure.step("Переход на страницу 'меню'"):
+            link = page.query_selector(selectors_menu_page.OUR_MENU_PAGE_LINK)
+            link.click()
+            page.wait_for_timeout(2000)
+        with allure.step("Проверка правильности перехода"):
+            title = page.query_selector(selectors_menu_page.OUR_MENU_PAGE_TITLE)
+            assert title.inner_text().upper() == "OUR MENU", "Link to our menu page is not working"
 
+    @allure.feature("Move to products page throw navigation menu")
+    @allure.story("Переход на страницу 'продукты' с помощью навигационного меню")
     def test_go_to_products_page(self, setup_browser):
-        page = setup_browser
-        link = page.query_selector(selectors_products_page.PRODUCTS_PAGE_LINK)
-        link.click()
-        page.wait_for_timeout(2000)
-        title = page.query_selector(selectors_products_page.PRODUCTS_PAGE_TITLE)
-        assert title.inner_text() == "PRODUCTS", "Link to products page is not working"
+        with allure.step("Запуск браузера и открытие страницы"):
+            page = setup_browser
+        with allure.step("Переход на страницу 'продукты'"):
+            link = page.query_selector(selectors_products_page.PRODUCTS_PAGE_LINK)
+            link.click()
+            page.wait_for_timeout(2000)
+        with allure.step("Проверка правильности перехода"):
+            title = page.query_selector(selectors_products_page.PRODUCTS_PAGE_TITLE)
+            assert title.inner_text().upper() == "PRODUCTS", "Link to products page is not working"
 
+    @allure.feature("Move to about page throw navigation menu")
+    @allure.story("Переход на страницу 'о нас' с помощью навигационного меню")
     def test_go_to_about_page(self, setup_browser):
-        page = setup_browser
-        link = page.query_selector(selectors_about_page.ABOUT_PAGE_LINK)
-        link.click()
-        page.wait_for_timeout(2000)
-        title = page.query_selector(selectors_about_page.ABOUT_PAGE_TITLE)
-        assert title.inner_text() == "ABOUT US", "Link to home page is not working"
+        with allure.step("Запуск браузера и открытие страницы"):
+            page = setup_browser
+        with allure.step("Переход на страницу 'о нас'"):
+            link = page.query_selector(selectors_about_page.ABOUT_PAGE_LINK)
+            link.click()
+            page.wait_for_timeout(2000)
+        with allure.step("Проверка правильности перехода"):
+            title = page.query_selector(selectors_about_page.ABOUT_PAGE_TITLE)
+            assert title.inner_text().upper() == "ABOUT US", "Link to about page is not working"
 
+    @allure.feature("Move to contact us page throw navigation menu")
+    @allure.story("Переход на страницу 'контакты' с помощью навигационного меню")
     def test_go_to_contactus_page(self, setup_browser):
-        page = setup_browser
-        link = page.query_selector(selectors_contact_page.CONTACTUS_PAGE_LINK)
-        link.click()
-        page.wait_for_timeout(2000)
-        title = page.query_selector(selectors_contact_page.CONTACTUS_PAGE_TITLE)
-        assert title.inner_text() == "CONTACT US", "Link to home page is not working"
+        with allure.step("Запуск браузера и открытие страницы"):
+            page = setup_browser
+        with allure.step("Переход на страницу 'контакты'"):
+            link = page.query_selector(selectors_contact_page.CONTACTUS_PAGE_LINK)
+            link.click()
+            page.wait_for_timeout(2000)
+        with allure.step("Проверка правильности перехода"):
+            title = page.query_selector(selectors_contact_page.CONTACTUS_PAGE_TITLE)
+            assert title.inner_text().upper() == "CONTACT US", "Link to contact us page is not working"
 
+    @allure.feature("Move to cart page throw navigation menu")
+    @allure.story("Переход на страницу 'корзина' с помощью навигационного меню")
     def test_go_to_cart_page(self, setup_browser):
-        page = setup_browser
-        link = page.query_selector(selectors_cart_page.CART_PAGE_LINK)
-        link.click()
-        page.wait_for_timeout(2000)
-        title = page.query_selector(selectors_cart_page.CART_PAGE_TITLE)
-        assert title.inner_text() == "MY CART", "Link to home page is not working"
+        with allure.step("Запуск браузера и открытие страницы"):
+            page = setup_browser
+        with allure.step("Переход на страницу 'корзина'"):
+            link = page.query_selector(selectors_cart_page.CART_PAGE_LINK)
+            link.click()
+            page.wait_for_timeout(2000)
+        with allure.step("Проверка правильности перехода"):
+            title = page.query_selector(selectors_cart_page.CART_PAGE_TITLE)
+            assert title.inner_text().upper() == "MY CART", "Link to cart page is not working"
 
